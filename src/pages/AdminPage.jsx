637c26d88dbef761aab8c8080233c4706160e490
Sevi68 <sevindupunsara313@gmail.com>
2024-08-21 01:10:03 +0530

    add admin page

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,14 @@
+import AdminNavBar from "../components/AdminNavBar";
+import BusRegistrationForm from "../components/BusRegisterationForm";
+
+const AdminPage = () => {
+
+    return (
+        <div className="grid grid-cols-[1fr_5fr] gap-4">
+           <div className="border-r-2"><AdminNavBar/></div> 
+           <div><BusRegistrationForm /></div> 
+        </div>
+    );
+};
+
+export default AdminPage;
