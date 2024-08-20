8edc28c678f9d0cdbbf41c63d9643b8f7802323f
Sevi68 <sevindupunsara313@gmail.com>
2024-08-21 01:19:57 +0530

    add admin navbar

---
+++ src/components/AdminNavBar.jsx
@@ -0,0 +1,15 @@
+const AdminNavBar = () => {
+
+    return (
+        <div className="grid">
+            <button className="py-2 border-b-2">Add Bus</button>
+            <button className="py-2 border-b-2">Add Owner</button>
+            <button className="py-2 border-b-2">Add Route</button>
+            <button className="py-2 border-b-2">View Users</button>
+            <button className="py-2 border-b-2">View Bus</button>
+            <button className="py-2 border-b-2">View Routes</button>
+        </div>
+    );
+};
+
+export default AdminNavBar;
