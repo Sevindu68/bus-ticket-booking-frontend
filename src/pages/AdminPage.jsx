606dee3c430fb4ce21dcf02b68b64e31096c37b2
Sevi68 <sevindupunsara313@gmail.com>
2024-08-21 01:20:31 +0530

    change css

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,7 +4,7 @@ import BusRegistrationForm from "../components/BusRegisterationForm";
 const AdminPage = () => {
 
     return (
-        <div className="grid grid-cols-[1fr_5fr] gap-4">
+        <div className="grid grid-cols-[1fr_5fr] gap-4 p-4">
            <div className="border-r-2"><AdminNavBar/></div> 
            <div><BusRegistrationForm /></div> 
         </div>
