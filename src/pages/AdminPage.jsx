53b47ae2443e7f817a59d333501b806156edb3c8
Sevi68 <sevindupunsara313@gmail.com>
2024-08-21 23:17:47 +0530

    change stylings

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -5,7 +5,7 @@ const AdminPage = () => {
 
     return (
         <div className="grid grid-cols-[1fr_5fr] gap-4 p-4">
-           <div className="border-r-2"><AdminNavBar/></div> 
+           <div className="border-2"><AdminNavBar/></div> 
            <div><BusRegistrationForm /></div> 
         </div>
     );
